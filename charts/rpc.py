f95c956bbcaaae6e9ebbe9bb8f04c60f9874417a
Pavol Rusnak <stick@gk2.sk>
2017-09-10 17:44:58 +0900

    default to rawproxy w/o params if no rpc_crendentials found

--- charts/rpc.py
+++ charts/rpc.py
@@ -1,5 +1,7 @@
 import bitcoin.rpc
 
-from rpc_credentials import RPC_HOST, RPC_PORT, RPC_USER, RPC_PASS
-
-rpc = bitcoin.rpc.RawProxy(service_url='http://%s:%s@%s:%d' % (RPC_USER, RPC_PASS, RPC_HOST, RPC_PORT))
+try:
+    from rpc_credentials import RPC_HOST, RPC_PORT, RPC_USER, RPC_PASS
+    rpc = bitcoin.rpc.RawProxy(service_url='http://%s:%s@%s:%d' % (RPC_USER, RPC_PASS, RPC_HOST, RPC_PORT))
+except:
+    rpc = bitcoin.rpc.RawProxy()

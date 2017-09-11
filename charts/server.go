f837fc22622cce8be249ae190a3ca1b8e252014c
Pavol Rusnak <stick@gk2.sk>
2017-09-11 15:56:21 +0900

    add logger to server

--- charts/server.go
+++ charts/server.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"github.com/gorilla/handlers"
 	"github.com/rs/cors"
 	"net/http"
+	"os"
 )
 
 func main() {
 	fs := http.FileServer(http.Dir("."))
-	handler := cors.Default().Handler(fs)
+	logger := handlers.CombinedLoggingHandler(os.Stdout, fs)
+	handler := cors.Default().Handler(logger)
 	http.ListenAndServe(":8080", handler)
 }

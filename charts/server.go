af650221481f5955614c7263b15b144351576f6a
Pavol Rusnak <stick@gk2.sk>
2017-09-12 03:28:25 +0900

    update server

--- charts/server.go
+++ charts/server.go
@@ -7,9 +7,15 @@ import (
 	"os"
 )
 
+func sendData(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "data.json")
+}
+
 func main() {
-	fs := http.FileServer(http.Dir("."))
-	logger := handlers.CombinedLoggingHandler(os.Stdout, fs)
-	handler := cors.Default().Handler(logger)
-	http.ListenAndServe(":8080", handler)
+	mux := http.NewServeMux()
+	mux.HandleFunc("/data.json", sendData)
+	cors := cors.Default().Handler(mux)
+	compressed := handlers.CompressHandler(cors)
+	logger := handlers.LoggingHandler(os.Stdout, compressed)
+	http.ListenAndServe(":8080", logger)
 }
